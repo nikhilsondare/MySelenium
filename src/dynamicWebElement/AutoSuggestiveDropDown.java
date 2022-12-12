a89b3ae37c0dd5edf884046142a4f7e7e0ba6667
Nikhil Sondare <nikhilsondare@gmail.com>
2022-12-12 17:01:21 +0530

    added spaces

--- src/dynamicWebElement/AutoSuggestiveDropDown.java
+++ src/dynamicWebElement/AutoSuggestiveDropDown.java
@@ -18,6 +18,7 @@ public class AutoSuggestiveDropDown {
 		driver.get("https://vctcpune.com/selenium/practice.html");
 		//List<WebElement> result1 = driver.findElements(By.id("mah-district"));
 		Thread.sleep(500);
+		
 		WebElement dropDown = driver.findElement(By.xpath("//input[@list='mah-district']"));
 		dropDown.click();
 		List<WebElement> result1 = driver.findElements(By.xpath("(//div[@class=' extradiv col-lg-8 col-md-12 col-12'])//datalist/option"));
